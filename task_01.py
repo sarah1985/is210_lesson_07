c3940824f4566c85d6f4c6f787fe2ab63ad706e0
sarah1985 <sarahmleavy@gmail.com>
2014-10-07 23:54:37 -0400

    task 1

---
+++ task_01.py
@@ -0,0 +1,25 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Task 01: Creating a Dictionary """
+
+GRADE_DATA = {
+    "Luke Skywalker": {
+        "math": "B",
+        "etiquette": "B+",
+        "grammar": "B",
+        "gym": "A"
+    },
+    "Han Solo": {
+        "math": "A-",
+        "etiquette": "C-",
+        "grammar": "B",
+        "gym": "B"
+    },
+    "C-3PO": {
+        "math": "C",
+        "etiquette": "A+",
+        "grammar": "A",
+        "gym": "F"
+    }
+}
+print GRADE_DATA
